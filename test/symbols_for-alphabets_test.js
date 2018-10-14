fe588fabab509e619e3acd385526744bd49fe69b
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-10-14 11:06:07 +0530

    Handling scenario for whole character at once
    Added test case for whole character

--- test/symbols_for-alphabets_test.js
+++ test/symbols_for-alphabets_test.js
@@ -7,6 +7,15 @@ const symbol_for_alphabets_height_1 = function (expectedOutput, actualOutput) {
 const symbol_for_alphabets_height_2 = function (expectedOutput, actualOutput) { 
   assert.equal(lib.second(expectedOutput),actualOutput);
 }
+const symbol_for_alphabets_height_3 = function (expectedOutput, actualOutput) { 
+  assert.equal(lib.third(expectedOutput),actualOutput);
+}
+const symbol_for_alphabets_height_4 = function (expectedOutput, actualOutput) { 
+  assert.equal(lib.fourth(expectedOutput),actualOutput);
+}
+const symbol_for_alphabets_height_5 = function (expectedOutput, actualOutput) { 
+  assert.equal(lib.fifth(expectedOutput),actualOutput);
+}
 
 // for only one set of symbols in one function
 symbol_for_alphabets_height_1(0,'*****');
@@ -19,6 +28,12 @@ symbol_for_alphabets_height_1(1,'*****');
 symbol_for_alphabets_height_1(0,'*****');
 symbol_for_alphabets_height_2(0,'*   *');
 
+// one whole character
+symbol_for_alphabets_height_1(0,'*****');
+symbol_for_alphabets_height_2(0,'*   *');
+symbol_for_alphabets_height_3(0,'*****');
+symbol_for_alphabets_height_4(0,'*   *');
+symbol_for_alphabets_height_5(0,'*   *');
 
 
 
