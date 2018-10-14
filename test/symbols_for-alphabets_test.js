bc9b7c37481309ad3c275eb96a0a07b820aec139
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-10-14 10:49:26 +0530

    handle two symbol sets from two functions.
    Added scenario for two symbol sets from two functions in a same file.
    Both symbol sets are a part of one character representation ("A").

--- test/symbols_for-alphabets_test.js
+++ test/symbols_for-alphabets_test.js
@@ -4,6 +4,9 @@ let assert = require('assert');
 const symbol_for_alphabets_height_1 = function (expectedOutput, actualOutput) { 
   assert.equal(lib.first(expectedOutput),actualOutput);
 }
+const symbol_for_alphabets_height_2 = function (expectedOutput, actualOutput) { 
+  assert.equal(lib.second(expectedOutput),actualOutput);
+}
 
 // for only one set of symbols in one function
 symbol_for_alphabets_height_1(0,'*****');
@@ -12,6 +15,9 @@ symbol_for_alphabets_height_1(0,'*****');
 symbol_for_alphabets_height_1(0,'*****');
 symbol_for_alphabets_height_1(1,'*****');
 
+// for two sets of symbols from different functions
+symbol_for_alphabets_height_1(0,'*****');
+symbol_for_alphabets_height_2(0,'*   *');
 
 
 
