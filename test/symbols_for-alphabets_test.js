6deddb0a4af5b84c27e7fc8aff51359f0f8db93d
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-10-14 10:40:07 +0530

    Handling scenario for two symbols sets
    test added for two symbol set and comments added in lib and test.

--- test/symbols_for-alphabets_test.js
+++ test/symbols_for-alphabets_test.js
@@ -1,8 +1,19 @@
 let lib = require('../lib/symbol_for_alphabets.js');
 let assert = require('assert');
 
-const symbol_for_alphabets_height_1 = function (expectedOutput, output) { 
-  assert.equal(lib.first(expectedOutput),output);
+const symbol_for_alphabets_height_1 = function (expectedOutput, actualOutput) { 
+  assert.equal(lib.first(expectedOutput),actualOutput);
 }
+
+// for only one set of symbols in one function
 symbol_for_alphabets_height_1(0,'*****');
+
+// for two sets of symbols in one function
+symbol_for_alphabets_height_1(0,'*****');
+symbol_for_alphabets_height_1(1,'*****');
+
+
+
+
+
 console.log('Test Passed');
